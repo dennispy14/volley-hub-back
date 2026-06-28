a1ba8bba5511224c98df8d22ba43971491765c30
Admin <dennispy14@hotmail.com>
2026-06-27 21:16:58 -0300

    fix: update database connection properties for improved access

--- src/main/java/com/br/volleyhub/VolleyHubApplication.java
+++ src/main/java/com/br/volleyhub/VolleyHubApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VolleyHubApplication {
+public class
+VolleyHubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VolleyHubApplication.class, args);
